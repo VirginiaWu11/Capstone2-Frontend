6ef4f02c1cf0200f67684995d51c131c1802dd6d
Virginia Wu <virginiawu11@gmail.com>
2021-12-19 20:55:25 -0500

    set up register, signin, updateProfile routes

---
+++ src/api.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
+
+/** API Class. */
+
+class BackendApi {
+  // token stored to interact with the backend API.
+  static token;
+
+  static async request(endpoint, data = {}, method = "get") {
+    console.debug("API Call:", endpoint, data, method);
+
+    const url = `${BASE_URL}/${endpoint}`;
+    const headers = { Authorization: `Bearer ${BackendApi.token}` };
+    const params = method === "get" ? data : {};
+
+    try {
+      return (await axios({ url, method, data, params, headers })).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  // Individual API routes
+
+  /** Log in. */
+  static async signin(data) {
+    let res = await this.request(`auth/login`, data, "post");
+    return res.token;
+  }
+
+  /** Sign up. */
+  static async register(data) {
+    let res = await this.request(`auth/register`, data, "post");
+    return res.token;
+  }
+
+  static async getCurrentUser(username) {
+    let res = await this.request(`users/${username}`);
+    return res.user;
+  }
+
+  static async updateProfile(username, data) {
+    let res = await this.request(`users/${username}`, data, "patch");
+    return res.user;
+  }
+}
+
+export default BackendApi;
