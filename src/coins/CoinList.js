ef7d52b854197da91b9d76b67ccd1a70e82e2acb
Virginia Wu <virginiawu11@gmail.com>
2021-12-31 14:39:16 -0500

    add toggle table or grid view

--- src/coins/CoinList.js
+++ src/coins/CoinList.js
@@ -11,12 +11,18 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import ViewListIcon from "@mui/icons-material/ViewList";
+import ViewModuleIcon from "@mui/icons-material/ViewModule";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import CoinTable from "./CointTable";
 
 const CoinList = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [coins, setCoins] = useState([]);
   const [page, setPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(20);
+  const [view, setView] = useState("module");
 
   useEffect(() => {
     const getCoins = async (page, itemsPerPage) => {
@@ -30,6 +36,28 @@ const CoinList = () => {
   if (isLoading) {
     return <p>Loading &hellip;</p>;
   }
+
+  const VerticalToggleButtons = () => {
+    const handleChange = (event, nextView) => {
+      setView(nextView);
+    };
+
+    return (
+      <ToggleButtonGroup
+        value={view}
+        exclusive
+        onChange={handleChange}
+        sx={{ ml: 1, mt: 2 }}
+      >
+        <ToggleButton value="list" aria-label="list">
+          <ViewListIcon />
+        </ToggleButton>
+        <ToggleButton value="module" aria-label="module">
+          <ViewModuleIcon />
+        </ToggleButton>
+      </ToggleButtonGroup>
+    );
+  };
 
   const BasicSelect = () => {
     const handleChange = (event) => {
@@ -77,13 +105,18 @@ const CoinList = () => {
   return (
     <div>
       <Container maxWidth="xl">
+        <VerticalToggleButtons />
         <Grid container spacing={1} justifyContent="center" pt={2}>
-          {coins.length ? (
-            <CoinsCardList coins={coins} />
+          {view === "module" ? (
+            coins.length ? (
+              <CoinsCardList coins={coins} />
+            ) : (
+              <Grid item xs={3}>
+                <p>No coins found</p>
+              </Grid>
+            )
           ) : (
-            <Grid item xs={3}>
-              <p>No coins found</p>
-            </Grid>
+            <CoinTable coins={coins} />
           )}
           <BasicSelect />
           <PaginationOutlined />
