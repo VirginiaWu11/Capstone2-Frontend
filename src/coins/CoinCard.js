f8e459f9b1bc2fcd877d113b87529c3f2217b497
Virginia Wu <virginiawu11@gmail.com>
2022-01-26 18:42:36 -0500

    truncate Coin Name and Coin Symbol

--- src/coins/CoinCard.js
+++ src/coins/CoinCard.js
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, useCallback } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -6,6 +6,10 @@ import Box from "@mui/material/Box";
 import CardMedia from "@mui/material/CardMedia";
 
 const CoinCard = memo(({ coin }) => {
+  const truncate = useCallback((str, n) => {
+    return str.length > n ? str.substr(0, n - 1) + "..." : str;
+  }, []);
+
   return (
     <Card
       sx={{ minWidth: 275, display: "flex", justifyContent: "space-between" }}
@@ -18,10 +22,10 @@ const CoinCard = memo(({ coin }) => {
       >
         <CardContent>
           <Typography variant="h5" component="div">
-            {coin.name}
+            {truncate(coin.name, 15)}
           </Typography>
           <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            {coin.symbol}
+            {truncate(coin.symbol, 20)}
           </Typography>
           {coin.current_price ? (
             <Typography variant="body2">{`Current Price: $${coin.current_price.toLocaleString()}`}</Typography>
